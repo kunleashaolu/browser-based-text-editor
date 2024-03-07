3b56604c8feea4f82cc7f044a088c8749fb2fe4b
Adekunle Ashaolu <dekxnle@gmail.com>
2024-03-07 21:44:19 +0100

    mini gradient clock

---
+++ script.js
@@ -0,0 +1,16 @@
+const clock = document.getElementById('clock')
+
+function setDate() {
+  // Code to set the current time and hand angles.
+  const now = new Date()
+  const secondsAngle = now.getSeconds() * 6
+  const minsAngle = now.getMinutes() * 6 + secondsAngle / 60
+  const hourAngle = ((now.getHours() % 12) / 12) * 360 + minsAngle / 12
+
+  clock.style.setProperty('--second-hand-degrees', secondsAngle + 'deg')
+  clock.style.setProperty('--minute-hand-degrees', minsAngle + 'deg')
+  clock.style.setProperty('--hour-hand-degrees', hourAngle + 'deg')
+}
+
+setInterval(setDate, 1000)
+setDate()
