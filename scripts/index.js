d0194e563b42c5ac4045e56aa53a77d817eece97
Adekunle Ashaolu <dekxnle@gmail.com>
2024-03-26 22:39:11 +0100

    .

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,11 @@
-import { setTheme } from './theme'
 import { startClock } from './clock'
+
+startClock()
+
+const screen = document.firstElementChild
+function setTheme(theme) {
+  screen.setAttribute('color-scheme', theme)
+}
 
 const themeSwitch = document.getElementById('theme-switch')
 themeSwitch.addEventListener('change', (e) => (e.target.checked ? setTheme('dark') : setTheme('light')))
-
-startClock()
--- scripts/theme.js
+++
@@ -1,5 +0,0 @@
-const screen = document.firstElementChild
-
-export function setTheme(theme) {
-  screen.setAttribute('color-scheme', theme)
-}
