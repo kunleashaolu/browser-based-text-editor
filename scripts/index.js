32121ef8211da10f0501da1038f727573324f185
Adekunle Ashaolu <dekxnle@gmail.com>
2024-04-03 18:34:59 +0100

    floating menu

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,19 @@
-import { startClock } from './clock'
+const clock = document.getElementById('clock')
 
-startClock()
+function setDate() {
+  // Code to set the current time and hand angles.
+  const now = new Date()
+  const secondsAngle = now.getSeconds() * 6
+  const minsAngle = now.getMinutes() * 6 + secondsAngle / 60
+  const hourAngle = ((now.getHours() % 12) / 12) * 360 + minsAngle / 12
+
+  clock.style.setProperty('--second-hand-degrees', secondsAngle + 'deg')
+  clock.style.setProperty('--minute-hand-degrees', minsAngle + 'deg')
+  clock.style.setProperty('--hour-hand-degrees', hourAngle + 'deg')
+}
+
+setInterval(setDate, 1000)
+setDate()
 
 const screen = document.firstElementChild
 function setTheme(theme) {
