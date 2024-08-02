5a2f2f2f72c565e6e650a75ca56eb3bae35717a7
kunleashaolu <kxnleashaolu@gmail.com>
2024-08-02 20:05:29 +0100

    sidenav

--- scripts/index.js
+++ scripts/index.js
@@ -10,3 +10,48 @@ fontStyleOptions.addEventListener('change', (e) => console.log(e))
 
 const themeSwitch = document.getElementById('theme-switch')
 themeSwitch.addEventListener('change', (e) => (e.target.checked ? setTheme('dark') : setTheme('light')))
+
+// const nav = document.querySelector('nav')
+
+let previousScrollPosition = 0
+
+const isScrollingDown = () => {
+  let scrolledPosition = document.body.scrollTop
+  let isScrollDown
+
+  if (scrolledPosition > previousScrollPosition) {
+    isScrollDown = true
+  } else {
+    isScrollDown = false
+  }
+  previousScrollPosition = scrolledPosition
+  return isScrollDown
+}
+
+// const handleNavScroll = () => {
+//   if (isScrollingDown() && !nav.contains(document.activeElement)) {
+//     nav.classList.add('scroll-down')
+//     nav.classList.remove('scroll-up')
+//   } else {
+//     nav.classList.add('scroll-up')
+//     nav.classList.remove('scroll-down')
+//   }
+// }
+
+// var throttleTimer
+
+// const throttle = (callback, time) => {
+//   if (throttleTimer) return
+
+//   throttleTimer = true
+//   setTimeout(() => {
+//     callback()
+//     throttleTimer = false
+//   }, time)
+// }
+
+// const mediaQuery = window.matchMedia('(prefers-reduced-motion: reduce)')
+
+window.addEventListener('scroll', () => {
+  console.log(isScrollingDown)
+})
