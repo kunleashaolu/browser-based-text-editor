9bb1362854220828324bc57666e71e9c4158d435
kunleashaolu <kxnleashaolu@gmail.com>
2024-08-11 22:07:14 +0100

    utility functions

---
+++ scripts/utils.js
@@ -0,0 +1,17 @@
+export function copy() {}
+
+export function save() {}
+
+export function toUppercase() {}
+
+export function toLowercase() {}
+
+export function textUnderline() {}
+
+export function textCrossover() {}
+
+export function textBold() {}
+
+export function textAlign(position) {}
+
+export function textStyle() {}
