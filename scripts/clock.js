c45bc25b1b4b5c422061dbfaed87f92db97526a4
kunleashaolu <kxnleashaolu@gmail.com>
2025-01-11 16:12:24 +0100

    fix: typo

--- scripts/clock.js
+++ scripts/clock.js
@@ -4,11 +4,11 @@ function setDate(e) {
   // Code to set the current time and hand angles.
   const now = new Date()
   const secondsAngle = now.getSeconds() * 6
-  const minsAngle = now.getMinutes() * 6 + secondsAngle / 60
-  const hourAngle = ((now.getHours() % 12) / 12) * 360 + minsAngle / 12
+  const minAngle = now.getMinutes() * 6 + secondsAngle / 60
+  const hourAngle = ((now.getHours() % 12) / 12) * 360 + minAngle / 12
 
   e.style.setProperty('--second-hand-degrees', secondsAngle + 'deg')
-  e.style.setProperty('--minute-hand-degrees', minsAngle + 'deg')
+  e.style.setProperty('--minute-hand-degrees', minAngle + 'deg')
   e.style.setProperty('--hour-hand-degrees', hourAngle + 'deg')
 }
 
